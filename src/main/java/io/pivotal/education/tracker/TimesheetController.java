149e2319d94f136472cd361481eb07a3bfd97640
Bill Kable <bkable@pivotal.io>
2019-11-26 12:15:23 -0600

    timesheet-create

---
+++ src/main/java/io/pivotal/education/tracker/TimesheetController.java
@@ -0,0 +1,22 @@
+package io.pivotal.education.tracker;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.ResponseEntity.*;
+
+@RestController
+@RequestMapping("/timesheets")
+public class TimesheetController {
+    private final TimesheetRepository repository;
+
+    public TimesheetController(TimesheetRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Timesheet> create(@RequestBody Timesheet timesheetToCreate) {
+        return created(null)
+                .body(repository.save(timesheetToCreate));
+    }
+}
